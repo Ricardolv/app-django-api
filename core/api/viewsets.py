1819ca675fe499a3a89fe05f5b5c445ba7368114
Ricardolv <richardluizv@gmail.com>
2019-05-11 20:44:17 -0300

    Utilizando o SearchFilter

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
@@ -9,6 +10,8 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     # queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('nome', 'descricao', '=endereco__linha1')
 
     def get_queryset(self):
         return self.__query_set()
