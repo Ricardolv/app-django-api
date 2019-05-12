0f2e4611318747625a1c6fa39427910a3db80d79
Ricardolv <richardluizv@gmail.com>
2019-05-12 02:19:23 -0300

    ajust

--- comentarios/api/viewsets.py
+++ comentarios/api/viewsets.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Comentario
 from .serializers import ComentarioSerializer
 
 
 class ComentarioViewSet(ModelViewSet):
-
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
--- enderecos/api/viewsets.py
+++ enderecos/api/viewsets.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Endereco
 from .serializers import EnderecoSerializer
 
 
 class EnderecoViewSet(ModelViewSet):
-
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
