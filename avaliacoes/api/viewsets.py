3bb4d3d08375dce0f230d670b1122cfb6877966e
Ricardolv <richardluizv@gmail.com>
2019-05-12 02:16:20 -0300

    ajust

--- avaliacoes/api/viewsets.py
+++ avaliacoes/api/viewsets.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Avaliacao
 from .serializers import AvaliacaoSerializer
 
 
 class AvaliacaoViewSet(ModelViewSet):
-
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
