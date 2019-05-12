3b9a3aa556376bbf114234ee1a683952c57ae230
Ricardolv <richardluizv@gmail.com>
2019-05-12 03:49:32 -0300

    Relacionando objetos com objetos existentes via ID

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
-from core.models import PontoTuristico
+from core.models import PontoTuristico, DocIdentificacao
 from atracoes.models import Atracao
 from enderecos.models import Endereco
 
@@ -9,17 +9,24 @@ from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 
 
+class DocIdentificacaoSerializer(ModelSerializer):
+    class Meta:
+        model = DocIdentificacao
+        fields = '__all__'
+
+
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer(read_only=True)
     decrisao_completa = SerializerMethodField()
+    doc_identificacao = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = (
             'id', 'nome', 'descricao', 'aprovado', 'foto',
             'atracoes', 'comentarios', 'avaliacoes', 'endereco',
-            'decrisao_completa', 'descricao_completa2'
+            'decrisao_completa', 'descricao_completa2', 'doc_identificacao'
         )
         read_only_fields = ('comentarios', 'avaliacoes')
 
@@ -35,11 +42,16 @@ class PontoTuristicoSerializer(ModelSerializer):
         endereco = validated_data['endereco']
         del validated_data['endereco']
 
+        doc = validated_data['doc_identificacao']
+        del validated_data['doc_identificacao']
+        doci = DocIdentificacao.ojects.create(**doc)
+
         ponto = PontoTuristico.objects.create(**validated_data)
         self.cria_atracoes(atracoes, ponto)
 
         end = Endereco.objects.create(**endereco)
         ponto.endereco = end
+        ponto.doc_identificacao = doci
 
         return ponto
 
